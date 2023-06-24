d623f5b1b0833110f957e6634ada278a5e1d20a7
ArnonAsquira <arnonafking2010@gmail.com>
2023-06-24 16:38:59 +0300

    comitting progress

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,26 @@
 // Write your code here
+class Book {
+  constructor(title, author, ISBN, availableCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.availableCopies = availableCopies;
+  }
+
+  getAvailability() {
+    if (this.availableCopies === 0) {
+      return "out of stock";
+    } else if (this.availableCopies < 10) {
+      return "low stock";
+    }
+    return "in stock";
+  }
+
+  sell(numCopies = 1) {
+    this.availableCopies - numCopies;
+  }
+
+  restock(numCopies = 5) {
+    this.availableCopies += numCopies;
+  }
+}
--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,20 @@
 // Write your code here
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+
+  getOverView() {
+    console.log(`${this.title}, a ${this.genre} film directed by ${this.director}
+           , was released in ${this.releaseYear}.
+           it recieved rating of ${this.rating}`);
+  }
+}
+
+const old = new Movie("Old", "Shams Cheranya", "Horror", 2020, 6.3);
+
+old.getOverView();
